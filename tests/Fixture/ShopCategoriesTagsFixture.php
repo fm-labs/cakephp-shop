bf3b2971356cd17fbdab462be8a9dada4f679917
fm-labs <flowmotionlabs@gmail.com>
2017-06-16 21:23:02 +0200

    added fixtures

---
+++ tests/Fixture/ShopCategoriesTagsFixture.php
@@ -0,0 +1,26 @@
+<?php
+namespace Shop\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ShopCategoriesTagsFixture
+ *
+ */
+class ShopCategoriesTagsFixture extends TestFixture
+{
+
+    /**
+     * Import
+     *
+     * @var array
+     */
+    public $import = ['table' => 'shop_categories_tags'];
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [];
+}
---
+++ tests/Fixture/ShopTagsFixture.php
@@ -0,0 +1,26 @@
+<?php
+namespace Shop\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ShopTagsFixture
+ *
+ */
+class ShopTagsFixture extends TestFixture
+{
+
+    /**
+     * Import
+     *
+     * @var array
+     */
+    public $import = ['table' => 'shop_tags'];
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [];
+}
