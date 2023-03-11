ee8e0714d814f3f429cc465386ab0c651ba1bfe8
fm-labs <flowmotionlabs@gmail.com>
2023-03-11 22:11:51 +0100

    Updated transaction views

--- src/Controller/Admin/ShopOrderTransactionsController.php
+++ src/Controller/Admin/ShopOrderTransactionsController.php
@@ -37,6 +37,7 @@ class ShopOrderTransactionsController extends AppController
         }
 
         $this->set('fields', [
+            'created' => ['formatter' => 'datetime'],
             //'shop_order' => ['formatter' => ['related', 'nr_formatted']],
             'shop_order' => ['formatter' => ['link', function (ShopOrder $order, ShopOrderTransaction $entity) {
                 return [
@@ -49,7 +50,7 @@ class ShopOrderTransactionsController extends AppController
             'status__status' => ['label' => 'Status', 'formatter' => 'status', 'type' => 'object'],
             'ext_status', 'last_message', 'is_test',
         ]);
-        $this->set('fields.whitelist', ['shop_order', 'type', 'engine', 'currency_code', 'value', 'status', 'ext_status', 'last_message', 'is_test']);
+        $this->set('fields.whitelist', ['created', 'shop_order', 'type', 'engine', 'currency_code', 'value', 'status', 'ext_status', 'last_message', 'is_test']);
         $this->set('ajax', $dataUrl);
         $this->set('queryObj', $query);
 
@@ -65,76 +66,7 @@ class ShopOrderTransactionsController extends AppController
      */
     public function view($id = null)
     {
+        $this->set('related', ['ShopOrderTransactionNotifies', 'ShopOrders']);
         $this->Action->execute();
-    }
-
-    /**
-     * Add method
-     *
-     * @return void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $shopOrderTransaction = $this->ShopOrderTransactions->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $shopOrderTransaction = $this->ShopOrderTransactions->patchEntity($shopOrderTransaction, $this->request->getData());
-            if ($this->ShopOrderTransactions->save($shopOrderTransaction)) {
-                $this->Flash->success(__d('shop', 'The {0} has been saved.', __d('shop', 'shop order transaction')));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__d('shop', 'The {0} could not be saved. Please, try again.', __d('shop', 'shop order transaction')));
-            }
-        }
-        $shopOrders = $this->ShopOrderTransactions->ShopOrders->find('list', ['limit' => 200]);
-        $this->set(compact('shopOrderTransaction', 'shopOrders'));
-        $this->set('_serialize', ['shopOrderTransaction']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Shop Order Transaction id.
-     * @return void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Http\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $shopOrderTransaction = $this->ShopOrderTransactions->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $shopOrderTransaction = $this->ShopOrderTransactions->patchEntity($shopOrderTransaction, $this->request->getData());
-            if ($this->ShopOrderTransactions->save($shopOrderTransaction)) {
-                $this->Flash->success(__d('shop', 'The {0} has been saved.', __d('shop', 'shop order transaction')));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__d('shop', 'The {0} could not be saved. Please, try again.', __d('shop', 'shop order transaction')));
-            }
-        }
-        $shopOrders = $this->ShopOrderTransactions->ShopOrders->find('list', ['limit' => 200]);
-        $this->set(compact('shopOrderTransaction', 'shopOrders'));
-        $this->set('_serialize', ['shopOrderTransaction']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Shop Order Transaction id.
-     * @return void Redirects to index.
-     * @throws \Cake\Http\Exception\NotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $shopOrderTransaction = $this->ShopOrderTransactions->get($id);
-        if ($this->ShopOrderTransactions->delete($shopOrderTransaction)) {
-            $this->Flash->success(__d('shop', 'The {0} has been deleted.', __d('shop', 'shop order transaction')));
-        } else {
-            $this->Flash->error(__d('shop', 'The {0} could not be deleted. Please, try again.', __d('shop', 'shop order transaction')));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
